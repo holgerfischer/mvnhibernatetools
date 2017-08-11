d2e9195460aa2588799f416231715b375047cb8a
HolgerFischer <holger@hfischer.org>
2017-08-11 13:18:47 +0200

    remove crebase.sql

--- crebase.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public.md_product  ( 
-	id         	integer NOT NULL,
-	description	varchar(255) NOT NULL,
-	image_url  	varchar(255) NULL,
-	price      	numeric(19,2) NULL,
-	product_id 	varchar(255) NOT NULL,
-	version    	integer NULL,
-	PRIMARY KEY(id)
-)
